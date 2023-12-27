213c2c2551cb58447598bf8de9bdbf237829f60d
Samuseu <s67.freelance@gmail.com>
2023-12-27 22:48:06 +0300

    update JUnit

--- src/test/java/junit/FirstJUnitTest.java
+++ src/test/java/junit/FirstJUnitTest.java
@@ -1,22 +1,32 @@
 package junit;
 
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.*;
+
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+
 
 public class FirstJUnitTest {
+
+    @BeforeAll // запускается 1 раз перед всеми тестами нужен пример задать браузер разрешение и тд
+    static void beforeAll() {
+        Configuration.browser = "opera";
+    }
+
+    @AfterAll //запускается 1 раз после всех тестов, нужен, чтобы почистить и тд
+    static void afterAll() {
+    }
 
     @BeforeEach
         // перед каждым тестам
     void beforeEach() {
-
     }
 
     @AfterEach
         //после каждого теста
     void afterEach() {
-
     }
 
     @Test

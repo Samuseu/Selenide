58162aeddf1e4402bddc690315b9086ce2e02ebe
Samuseu <s67.freelance@gmail.com>
2023-12-31 13:30:22 +0300

    Page Object

--- src/test/java/com/demoqa/pages/RegistrationPage.java
+++ src/test/java/com/demoqa/pages/RegistrationPage.java
@@ -1,4 +1,14 @@
 package com.demoqa.pages;
 
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.Selenide.executeJavaScript;
+
 public class RegistrationPage {
+    public void openPage(){
+        open("/automation-practice-form");
+        $(".practice-form-wrapper").shouldHave(text("Student Registration Form"));
+        executeJavaScript("$('#fixedban').remove()");
+        executeJavaScript("$('footer').remove()");
+    }
 }
--- src/test/java/com/demoqa/tests/RegistrationTests.java
+++ src/test/java/com/demoqa/tests/RegistrationTests.java
@@ -24,55 +24,28 @@ public class RegistrationTests {
         String userLastName = "Samuseu";
         String userEmail = "qa@gmail.com";
 
-        open("/automation-practice-form");
-        $(".practice-form-wrapper").shouldHave(text("Student Registration Form"));
-
-        executeJavaScript("$('#fixedban').remove()");
-        executeJavaScript("$('footer').remove()");
+        new RegistrationPage().openPage();
 
         $("#firstName").setValue(userName);
         $("#lastName").setValue(userLastName);
         $("#userEmail").setValue(userEmail);
-
-        //Radio button
-//        $("label[for=gender-radio-1]").click();// хороший вариант можно просто [for=gender-radio-1]
-//        $("#gender-radio-1").parent().click(); //good
         $("#genterWrapper").$(byText("Male")).click();
-//        $(byText("Male")).click(); // not very good ищет первый элемент
-
-        //Number
         $("#userNumber").setValue("3733333333");
-
-        //Data calendar
         $("#dateOfBirthInput").click();
         $(".react-datepicker__month-select").selectOption("July");
-//        $(".react-datepicker__month-select").selectOptionByValue("5");
         $(".react-datepicker__year-select").selectOption("2009");
         $(".react-datepicker__day--030:not(react-datepicker__day--outside-month)").click();
-
-        //checkbox
         $("#subjectsInput").setValue("Maths").pressEnter();
-
-        //Radio button
         $("#hobbiesWrapper").$(byText("Sports")).click();
-
-        //Picture
-//        $("").uploadFile(new File("src/test/resources/img/1.png"));
-        $("#uploadPicture").uploadFromClasspath("img/1.png");// или так сокращение говорится что файл уже лежит в папке ресурс
-
+        $("#uploadPicture").uploadFromClasspath("img/1.png");
         $("#currentAddress").setValue("Current Address");
-
         $("#state").click();
         $("#stateCity-wrapper").$(byText("NCR")).click();
-
         $("#city").click();
         $("#stateCity-wrapper").$(byText("Delhi")).click();
-
         $("#submit").click();
-
         $(".modal-dialog").shouldHave(appear);
         $(".modal-content").shouldHave(text("Thanks for submitting the form"));
         $(".table-responsive").shouldHave(text(userName), text(userLastName), text(userEmail), text("3733333333"));
-//        sleep(2000);
     }
 }
